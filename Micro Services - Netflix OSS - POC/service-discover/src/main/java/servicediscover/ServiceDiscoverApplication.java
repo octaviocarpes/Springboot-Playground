f64c372757ee75d15497be179627b7b263ea3080
Octavio Carpes <octavio.carpes10@gmail.com>
2018-07-09 15:21:02 -0300

    Added dockerfile to service discovery service (eureka)

--- Micro Services - Netflix OSS - POC/service-discover/src/main/java/servicediscover/ServiceDiscoverApplication.java
+++ Micro Services - Netflix OSS - POC/service-discover/src/main/java/servicediscover/ServiceDiscoverApplication.java
@@ -1,4 +1,4 @@
-package servicediscover.servicediscover;
+package servicediscover;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
